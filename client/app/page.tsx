4e1dfc4ade709531e0c66a2a3cd04f58d3678192
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-09-29 18:47:08 +0400

    Add Sing Out button into main page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,12 @@
 import Calendar from "@/components/calendar";
 import AuthGate from "@/components/AuthGate";
+import SignOutButton from "@/components/auth-buttons";
 
 export default function Home() {
   return (
     <AuthGate>
       <main>
+        <SignOutButton />
         <Calendar />
       </main>
     </AuthGate>
