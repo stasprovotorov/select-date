c9a6d4070b5f8be01a17d8cb081a852f91a58c56
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-09-29 18:46:26 +0400

    Replace button into components dir

---
+++ client/components/auth-buttons.tsx
@@ -0,0 +1,12 @@
+export default function SignOutButton() {
+  return (
+    <div className="flex justify-center my-6">
+      <a
+        href="/auth/logout"
+        className="px-6 py-2 rounded-lg bg-gray-800 text-white font-semibold shadow-md hover:shadow-lg transition-all duration-200 active:scale-95 active:shadow-sm"
+      >
+        Выйти
+      </a>
+    </div>
+  );
+}
