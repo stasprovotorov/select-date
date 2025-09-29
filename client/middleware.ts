88ada3884bc20faea61d8c79efbc71b98b5c811b
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-09-29 16:21:38 +0400

    Add the Authentication Middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -7,6 +7,12 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"
-  ]
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
