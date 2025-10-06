508fe25476c5e3a35fb6d0aa4134d6319478f27f
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-10-06 19:24:46 +0400

    Add token receiving auth0.getAccessToken

--- client/app/api/calendar/route.ts
+++ client/app/api/calendar/route.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from 'next/server';
+import { auth0 } from '../../../lib/auth0';
 
 export async function POST(request: Request) {
   const data = await request.json();
+  const { token: accessToken } = await auth0.getAccessToken();
   // Here will be code to send POST request to Fast API when it will be ready
   // Now it's just a print in conlose
-  console.log("Данные календаря:", data);
+  console.log(accessToken);
+  console.log(data);
   return NextResponse.json({ received: data });
 }
