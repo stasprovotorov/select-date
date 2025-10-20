857e7d40317342762313d078d8bee27defabaf35
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-10-20 19:31:34 +0400

    Add post-request to Fast API (test)

--- client/app/api/calendar/route.ts
+++ client/app/api/calendar/route.ts
@@ -4,6 +4,16 @@ import { auth0 } from '../../../lib/auth0';
 export async function POST(request: Request) {
   const data = await request.json();
   const { token: accessToken } = await auth0.getAccessToken();
+
+  // Forward to FastAPI
+  const backendRes = await fetch("http://127.0.0.1:8000/calendar", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  });
+  
   // Here will be code to send POST request to Fast API when it will be ready
   // Now it's just a print in conlose
   console.log(accessToken);
