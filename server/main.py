0ccf74474aa588fd0b224549828c7a37c10999d6
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-10-31 18:40:47 +0400

    Some minimal for test client requests

--- server/main.py
+++ server/main.py
@@ -1,24 +1,37 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Header, status
 from pydantic import BaseModel
-from typing import Literal, Annotated
+from typing import Annotated
+
+app = FastAPI()
 
 
 class SelectedDate(BaseModel):
     year: int
     month: int
     day: int
-    color: str
-    textColor: str
+    color: str | None = None
+    text_color: str | None = None
 
 
-class CalendarChange(BaseModel):
-    date: SelectedDate
-    action: Literal['add', 'remove']
+@app.post('/calendar/{date}', status_code=status.HTTP_201_CREATED)
+async def calendar_select_date(
+    date: str, 
+    date_data: SelectedDate,
+    authorization: Annotated[str | None, Header()]
+):
+    print('FastAPI POST')
+    print(date)
+    print(authorization)
+    print(date_data)
+
+    return date_data
 
 
-app = FastAPI()
-
-@app.post('/calendar')
-async def calendar_change(request: Request, data: CalendarChange):
-    print('Body data:', data.model_dump(), sep='\n')
-    print('Header data:', request.headers, sep='\n')
+@app.delete('/calendar/{date}', status_code=status.HTTP_204_NO_CONTENT)
+async def calendar_deselect_date(
+    date: str,
+    authorization: Annotated[str | None, Header()]
+):
+    print('FastAPI DELETE')
+    print(date)
+    print(authorization)
