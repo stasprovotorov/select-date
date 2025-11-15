42f03c910f5571950c001d74df60959dc5761bf4
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-11-15 20:59:39 +0400

    Correct SelectedDateSchema according to input data from client

--- server/src/schemas.py
+++ server/src/schemas.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 class SelectedDateSchema(BaseModel):
     year: int = Field(ge=1)
-    month: int = Field(ge=1, le=12)
+    month: int = Field(ge=0, le=11)
     day: int = Field(ge=1, le=31)
     color: str = Field(min_length=1)
-    color_text: str = Field(min_length=1)
+    textColor: str = Field(min_length=1)
