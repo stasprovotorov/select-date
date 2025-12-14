bdfe6b4015360c5f175d4c805ea71b789470a7bf
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-12-14 22:51:58 +0400

    Create batch send route

---
+++ client/app/api/calendar/batch/route.ts
@@ -0,0 +1,36 @@
+import { getJwt } from '../../../../lib/api-get-jwt'
+
+const BACKEND_URL = process.env.FASTAPI_URL || ''
+if (!BACKEND_URL) {
+  throw new Error('Missing required environment variable FASTAPI_URL in .env.local')
+}
+
+export async function POST(request: Request): Promise<Response> {
+  const backendURL = `${BACKEND_URL}/batch`
+  const authToken = await getJwt()
+  const jsonBody = await new Response(request.body).json()
+  const strJsonBody = JSON.stringify({ batch: jsonBody })
+  const bodyLen = Buffer.byteLength(strJsonBody)
+  const headers = new Headers(request.headers)
+  
+  headers.set('Authorization', `Bearer ${authToken}`)
+  headers.set('Content-Type', 'application/json')
+  headers.set('Content-Length', String(bodyLen))
+
+  // const backendRes = await fetch(backendURL, {
+  //   method: request.method,
+  //   headers,
+  //   body: strJsonBody
+  // })
+
+  // return new Response(backendRes.body, { 
+  //     status: backendRes.status, 
+  //     headers: backendRes.headers
+  // })
+
+  return await fetch(backendURL, {
+    method: request.method,
+    headers,
+    body: strJsonBody
+  })
+}
