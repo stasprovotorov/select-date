b627dd99779e170068d185cbf4566d3109fcceb0
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-12-18 16:35:54 +0400

    Add models for batch sending

--- server/src/schemas.py
+++ server/src/schemas.py
@@ -5,5 +5,35 @@ class SelectedDateSchema(BaseModel):
     year: int = Field(ge=1)
     month: int = Field(ge=0, le=11)
     day: int = Field(ge=1, le=31)
-    color: str = Field(min_length=1)
-    textColor: str = Field(min_length=1)
+    color: str | None = Field(default=None, min_length=1)
+    textColor: str | None = Field(default=None, min_length=1)
+
+
+class DateBatchItem(BaseModel):
+    action: str
+    date: str
+    color: str | None = None
+    textColor: str | None = None
+
+
+class DateBatch(BaseModel):
+    batch: list[DateBatchItem]
+
+
+class ApiDateItemResult(BaseModel):
+    ok: bool
+    action: str
+    date: str
+    color: str | None = None
+    textColor: str | None = None
+    message: str | None = None
+
+
+class ApiDateBatchResultSuccessed(BaseModel):
+    ok: bool = True
+    results: list[ApiDateItemResult]
+
+
+class ApiDateBatchResultFailed(BaseModel):
+    ok: bool = False
+    message: str
