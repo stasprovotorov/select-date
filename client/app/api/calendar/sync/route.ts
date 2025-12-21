0430a97a0c1438d9c67e179ce7c8231df5667b06
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-12-21 16:46:30 +0400

    Create GET method for data sync for user

---
+++ client/app/api/calendar/sync/route.ts
@@ -0,0 +1,19 @@
+import { getJwt } from '../../../../lib/api-get-jwt'
+
+const BACKEND_URL = process.env.FASTAPI_URL || ''
+if (!BACKEND_URL) {
+  throw new Error('Missing required environment variable FASTAPI_URL in .env.local')
+}
+
+export async function GET(request: Request): Promise<Response> {
+  const backendURL = `${BACKEND_URL}/batch`
+  const authToken = await getJwt()
+  const headers = new Headers(request.headers)
+  
+  headers.set('Authorization', `Bearer ${authToken}`)
+
+  return await fetch(backendURL, {
+    method: request.method,
+    headers
+  })
+}
