2f5a017c300d8f1c707952a7be6b628fe8f35ed6
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-12-27 20:34:02 +0400

    Refactor Auth

--- client/components/auth.tsx
+++ client/components/auth.tsx
@@ -1,27 +1,26 @@
 "use client";
 
-import { useUser } from "@auth0/nextjs-auth0";
-import { useEffect } from "react";
+import { useUser } from "@auth0/nextjs-auth0"
+import { useEffect } from "react"
 
-export default function AuthGate({ children }: { children: React.ReactNode }) {
-  const { user, isLoading } = useUser();
+export default function Auth({ children }: { children: React.ReactNode }) {
+  const { user, isLoading } = useUser()
 
   useEffect(() => {
     if (!isLoading && !user) {
-      // Redirect to the login page
-      window.location.href = "/auth/login";
+      window.location.href = "/auth/login"
     }
-  }, [isLoading, user]);
+  }, [isLoading, user])
 
-  // While loading, show "Loading..."
   if (isLoading) {
     return (
       <div className="flex items-center justify-center min-h-screen">
-        Loading...
+        Authorization...
       </div>
-    );
+    )
   }
 
-  // If the user is authorized, we render the application
-  return <>{children}</>;
+  if (!user) return null 
+
+  return <>{children}</>
 }
