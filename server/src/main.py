810946d433ee869fdd66580299a3a41ee5ef6dbb
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-12-31 04:09:27 +0400

    Correct attribute of returning DatesByUserSchema from get_dates_by_user()

--- server/src/main.py
+++ server/src/main.py
@@ -4,7 +4,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends, Body
 
 from auth import requires_auth
-from schemas import DateBatchRequestSchema, DateBatchResponseSchema, DateOperationSchema, DatesByUserSchema
+from schemas import DateBatchRequestSchema, DateBatchResponseSchema, DateOperationSchema, DateItemSchema, DatesByUserSchema
 from database import DatabaseProvider as dp
 
 
@@ -22,10 +22,10 @@ async def get_dates_by_user(token: Annotated[str, Depends(requires_auth)]) -> Da
     user_id = token["sub"]
 
     try:
-        dates = await dp.get_dates_by_user(user_id)
+        dates: list[DateItemSchema] = await dp.get_dates_by_user(user_id)
     except Exception as err:
         return DatesByUserSchema(ok=False, message=repr(err))
-    return DatesByUserSchema(ok=True, dates=dates)
+    return DatesByUserSchema(ok=True, items=dates)
 
 
 @app.post("/api/v1/dates/batch", response_model=DateBatchResponseSchema)
