5533ee9c5865a1cb90a5c9e38fcfe40f75bbe5ac
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2026-01-10 00:32:49 +0400

    Create get dates by user route

---
+++ client/app/api/v1/users/me/dates/route.ts
@@ -0,0 +1,20 @@
+const SERVER_API_URL = process.env.SERVER_API_URL
+
+if (!SERVER_API_URL) {
+  throw new Error("Missing required environment variable: SERVER_API_URL.")
+}
+
+export async function GET(req: Request): Promise<Response> {
+  const url = `${SERVER_API_URL}/users/me/dates`
+  const cookie = req.headers.get("Cookie")
+  const headers = new Headers()
+  
+  if (cookie) {
+    headers.set("Cookie", cookie)
+  }
+  
+  return await fetch(url, { 
+    method: req.method, 
+    headers 
+  })
+}
