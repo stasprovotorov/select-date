fa141f66eaa336c29741771877141152053c2c94
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2026-01-19 19:42:15 +0400

    Refactor

--- server/src/app/calendar/dependencies.py
+++ server/src/app/calendar/dependencies.py
@@ -1,7 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from src.app.calendar.models import Base
 from src.app.calendar.repository.crud import SqlAlchemyCalendarRepository
-from src.app.config import settings as global_settings
 from src.app.database import async_db
 
 
