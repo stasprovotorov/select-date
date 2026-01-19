5318a44084ff2ad984fde62c731d43723e35055c
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2026-01-19 19:40:58 +0400

    Implement async_db in get_sqlalchemy_repository function

--- server/src/app/calendar/dependencies.py
+++ server/src/app/calendar/dependencies.py
@@ -2,10 +2,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from src.app.calendar.models import Base
 from src.app.calendar.repository.crud import SqlAlchemyCalendarRepository
 from src.app.config import settings as global_settings
+from src.app.database import async_db
 
 
 async def get_sqlalchemy_repository() -> SqlAlchemyCalendarRepository:
-    async_engine = create_async_engine(global_settings.DB_URL)
-    Base.metadata.create_all(async_engine)
-    async_session = async_sessionmaker(bind=async_engine)
-    return SqlAlchemyCalendarRepository(async_session=async_session())
+    return SqlAlchemyCalendarRepository(async_session=async_db.async_session())
