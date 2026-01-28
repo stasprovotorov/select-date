20cbfa6c96140e06bb6127c08cd6cd12fb2c2bcb
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2026-01-28 21:00:50 +0400

    Create AuthSession* exceptions.

--- server/src/app/auth/exceptions.py
+++ server/src/app/auth/exceptions.py
@@ -32,3 +32,33 @@ class TokenValidationError(HTTPException):
             status_code=HTTP_401_UNAUTHORIZED,
             detail=f"Failed to validate token: {detail}"
         )
+
+
+class AuthBaseError(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class AuthSessionError(AuthBaseError):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class AuthSessionGetError(AuthSessionError):
+    def __init__(self):
+        super().__init__("Failed to obtain session object from the server.")
+
+
+class AuthSessionSetError(AuthSessionError):
+    def __init__(self):
+        super().__init__("Failed to set session object in the server.")
+
+
+class AuthSessionDeleteError(AuthSessionError):
+    def __init__(self):
+        super().__init__("Failed to remove session object from the server.")
+
+
+class AuthSessionDeserializationError(AuthSessionError):
+    def __init__(self):
+        super().__init__("Failed to deserialize session object from the server.")
