6be4213bef3a8ce45c2a786f8e0f0479bdf239c9
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2026-01-28 18:27:50 +0400

    Create AsyncRedis object and connection check with error handling.

--- server/src/app/core/redis.py
+++ server/src/app/core/redis.py
@@ -1,9 +1,29 @@
-from redis.asyncio import Redis
-from src.app.core.config import settings as global_settings
+from redis.asyncio import Redis, ConnectionError
+from src.app.core.config import settings
 
 
-client = Redis(
-    host=global_settings.DB_REDIS_HOST, 
-    port=global_settings.DB_REDIS_PORT, 
-    decode_responses=global_settings.DB_REDIS_DECODE_RESPONSES
+class AsyncRedis:
+    def __init__(self, host: str, port: int, decode_responses: bool) -> None:
+        self.host = host
+        self.port = port
+        self.decode_responses = decode_responses
+        self.client = Redis(
+            host=self.host,
+            port=self.port,
+            decode_responses=self.decode_responses
+        )
+
+    async def check_redis_server_connection(self):
+        try:
+            await self.client.ping()
+            # Log it.
+        except ConnectionError:
+            # Log it.
+            raise
+
+
+async_redis = AsyncRedis(
+    host=settings.DB_REDIS_HOST, 
+    port=settings.DB_REDIS_PORT, 
+    decode_responses=settings.DB_REDIS_DECODE_RESPONSES
 )
