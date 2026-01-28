a465d4ea9739647bbbb81ae2912bd15be0c24b5a
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2026-01-28 21:01:58 +0400

    Add error handling in SessionService methods.

--- server/src/app/auth/sessions.py
+++ server/src/app/auth/sessions.py
@@ -1,17 +1,21 @@
-from typing import TYPE_CHECKING
 import json
 
+import redis
+
+from src.app.core.config import settings
+from src.app.core.redis import async_redis
 from src.app.auth.schemas import Session
 from src.app.auth.utils import get_session_id, get_session_key
-from src.app.core.config import settings
-from src.app.core.redis import client as redis_client
-
-if TYPE_CHECKING:
-    from redis import Redis
+from src.app.auth.exceptions import (
+    AuthSessionGetError, 
+    AuthSessionSetError,
+    AuthSessionDeleteError, 
+    AuthSessionDeserializationError
+)
 
 
 class SessionService:
-    def __init__(self, storage: "Redis") -> None:
+    def __init__(self, storage: redis.Redis) -> None:
         self.storage = storage
 
     async def create_session(self, user: dict) -> str:
@@ -19,25 +23,44 @@ class SessionService:
         session_key = get_session_key(session_id)
         session = Session(user=user)
 
-        await self.storage.set(
-            name=session_key,
-            value=session.model_dump_json(),
-            ex=settings.SESSION_TTL
-        )
+        try:
+            await self.storage.set(
+                name=session_key,
+                value=session.model_dump_json(),
+                ex=settings.SESSION_TTL
+            )
+        except redis.ConnectionError as error:
+            # Log it.
+            raise AuthSessionSetError from error
 
         return session_id
 
     async def get_session(self, session_id: str) -> dict | None:
         session_key = get_session_key(session_id)
-        session = await self.storage.get(session_key)
 
-        if session:
+        try:
+            session = await self.storage.get(session_key)
             session_dict = json.loads(session)
             return session_dict
+        except redis.ConnectionError as error:
+            # Log it.
+            raise AuthSessionGetError from error
+        except json.JSONDecodeError as error:
+            # Log it.
+            raise AuthSessionDeserializationError from error
     
     async def remove_session(self, session_id: str) -> None:
         session_key = get_session_key(session_id)
-        await self.storage.delete(session_key)
+
+        try:
+            entry_count = await self.storage.delete(session_key)
+            if entry_count == 0:
+                # Log it.
+                pass
+            # Log it.
+        except redis.ConnectionError as error:
+            # Log it.
+            raise AuthSessionDeleteError from error
 
 
-session_service = SessionService(redis_client)
+session_service = SessionService(storage=async_redis.client)
