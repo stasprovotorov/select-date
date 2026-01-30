f7c67f4e4871b2fdeba5bacea5714742c7532d90
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2026-01-30 21:35:03 +0400

    Add errors handling.

--- server/src/app/auth/dependencies.py
+++ server/src/app/auth/dependencies.py
@@ -1,9 +1,15 @@
 import secrets
+
 from fastapi import Depends, Query, Cookie
+
 from src.app.auth.sessions import SessionService, session_service
-from src.app.auth.exceptions import UserNotAuthorized, AuthStatesNotMatched
 from src.app.auth.client import fetch_jwks, fetch_token
 from src.app.auth.service import validate_jwt
+from src.app.auth.exceptions import (
+    AuthStateNotMatchError,
+    AuthSessionIdNotFound, 
+    AuthSessionError
+)
 
 
 async def get_session_service() -> SessionService:
@@ -14,19 +20,38 @@ async def require_auth(
     session_id: str | None = Cookie(None), 
     session_service: SessionService = Depends(get_session_service)
 ) -> dict:
-    session = await session_service.get_session(session_id)
-    if not session:
-        raise UserNotAuthorized()
-    return session["user"]
+    if not session_id:
+        # Log it.
+        raise AuthSessionIdNotFound
 
+    try:    
+        session = await session_service.get_session(session_id)
+    except AuthSessionError as error:
+        # Log it.
+        raise
 
-async def validate_state(state: str | None = Query(None), auth_state: str | None = Cookie(None)) -> None:
-    if not secrets.compare_digest(state, auth_state):
-        raise AuthStatesNotMatched()
-
-
-async def get_authorized_user(code: str):
-    jwks = await fetch_jwks()
-    jwt = await fetch_token(code)
-    user = validate_jwt(jwt, jwks)
+    user = session["user"]
+    # Log it.
     return user
+
+
+async def validate_state(
+    state: str | None = Query(None), 
+    auth_state: str | None = Cookie(None)
+) -> None:
+    if not secrets.compare_digest(state, auth_state):
+        # Log it.
+        raise AuthStateNotMatchError
+
+
+async def get_authorized_user(code: str) -> dict:
+    jwt = await fetch_token(code)
+    # Log it.
+
+    jwks = await fetch_jwks()
+    # Log it.
+
+    user_info = validate_jwt(jwt, jwks)
+    # Log it.
+    
+    return user_info
