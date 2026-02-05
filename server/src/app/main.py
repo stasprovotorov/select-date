d2b12ac31a6b1625b24ef5c0a73ea9c3a3698565
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2026-02-05 16:48:24 +0400

    Add check_redis_server_connection() to starting application.

--- server/src/app/main.py
+++ server/src/app/main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.app.core.logs import setup_logging
 from src.app.core.database import async_db
+from src.app.core.redis import async_redis
 from src.app.core.settings import settings
 from src.app.auth.router import router as auth_router
 from src.app.calendar.router import router as calendar_router
@@ -14,6 +15,7 @@ from src.app.calendar.router import router as calendar_router
 async def lifespan(app: FastAPI):
     setup_logging()
     await async_db.initialize_async_database()
+    await async_redis.check_redis_server_connection()
     yield
     await async_db.shutdown_async_database()
 
